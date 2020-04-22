c3f3aaae178868c556f9936741d064d6452248f1
Erick <erickportugaldesenvolvedor@gmail.com>
2020-04-22 08:21:35 -0300

    front end meu site

---
+++ site/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { ContatoComponent } from './contato/contato.component';
+
+
+const routes: Routes = [
+
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'sobre-nos', component: SobreNosComponent },
+  { path: 'produtos', component: ProdutosComponent },
+  { path: 'contato', component: ContatoComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
